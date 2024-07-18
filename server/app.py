d77e0686fbf41a7b1a92c98eb5d82fbf17c9ed47
daniel beyene <dazimore@gmail.com>
2024-07-18 10:59:53 +0300

    One-to-One and One-To-Many Relationships : Code-Along (CodeGrade)

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask 
 from flask_migrate import Migrate
 
 from models import db, Employee, Onboarding, Review
